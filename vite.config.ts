3cbd901deb3a81d9f78078f3564079bf63c6c7ee
Khovanskiy Alexander <a@khovanskiy.ru>
2022-12-06 13:31:45 +0300

    add plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,21 @@
 import { defineConfig } from 'vite'
+import legacy from '@vitejs/plugin-legacy'
+import { EsLinter, linterPlugin, TypeScriptLinter } from "vite-plugin-linter";
+
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  // config options
-})
+export default defineConfig((configEnv) => ({
+
+  plugins: [
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+    }),
+    linterPlugin({
+      include: ["./src/**/*.ts", "./src/**/*.tsx"],
+      linters: [new EsLinter({ configEnv: configEnv }),
+      new TypeScriptLinter()
+      ],
+    }),
+  ],
+
+}))
