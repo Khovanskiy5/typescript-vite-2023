c861be836f8371dd7e69c00dd8a2f8417f26a6cb
Khovanskiy Alexander <a@khovanskiy.ru>
2022-12-06 13:52:07 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import legacy from '@vitejs/plugin-legacy'
-import { EsLinter, linterPlugin, TypeScriptLinter } from "vite-plugin-linter";
+//import { EsLinter, linterPlugin, TypeScriptLinter } from "vite-plugin-linter";
 
 
 // https://vitejs.dev/config/
@@ -10,12 +10,13 @@ export default defineConfig((configEnv) => ({
     legacy({
       targets: ['defaults', 'not IE 11'],
     }),
-    linterPlugin({
-      include: ["./src/**/*.ts", "./src/**/*.tsx"],
-      linters: [new EsLinter({ configEnv: configEnv }),
-      new TypeScriptLinter()
-      ],
-    }),
+//    linterPlugin({
+//      include: ["./src/**/*.ts", "./src/**/*.tsx"],
+//      linters: [
+//        new EsLinter({ configEnv: configEnv }),
+//        new TypeScriptLinter()
+//      ],
+//    }),
   ],
 
 }))
