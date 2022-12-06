8b2f7aa8861f577a9de127a907f04715bce68048
Khovanskiy Alexander <a@khovanskiy.ru>
2022-12-06 15:59:14 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,54 @@
-import { hello } from './modules/analytics.js'
-alert('Hello main.ts')
-hello()
 
-const message = 'hello'
+/**
+ * Класс Одиночка предоставляет метод getInstance, который позволяет клиентам
+ * получить доступ к уникальному экземпляру одиночки.
+ */
+ export class Singleton {
+    private static instance: Singleton;
 
-console.log(message)
+    /**
+     * Конструктор Одиночки всегда должен быть скрытым, чтобы предотвратить
+     * создание объекта через оператор new.
+     */
+    private constructor() { 
+        return Singleton.instance;
+    }
+
+    /**
+     * Статический метод, управляющий доступом к экземпляру одиночки.
+     *
+     * Эта реализация позволяет вам расширять класс Одиночки, сохраняя повсюду
+     * только один экземпляр каждого подкласса.
+     */
+    public static getInstance(): Singleton {
+        if (!Singleton.instance) {
+            Singleton.instance = new Singleton();
+        }
+
+        return Singleton.instance;
+    }
+
+    /**
+     * Наконец, любой одиночка должен содержать некоторую бизнес-логику, которая
+     * может быть выполнена на его экземпляре.
+     */
+    public someBusinessLogic() {
+        // ...
+    }
+}
+
+/**
+ * Клиентский код.
+ */
+function clientCode() {
+    const s1 = Singleton.getInstance();
+    const s2 = Singleton.getInstance();
+
+    if (s1 === s2) {
+        console.log('Singleton works, both variables contain the same instance.');
+    } else {
+        console.log('Singleton failed, variables contain different instances.');
+    }
+}
+
+clientCode();
